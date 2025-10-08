7f38a09522652bbfbf0b7b814455f1d97aed6824
Adam Wentz <adam.k.wentz@gmail.com>
2025-10-08 15:45:38 -0400

    Switch Square environment from Sandbox to Production

--- misas-thai-api/TakePayment.cs
+++ misas-thai-api/TakePayment.cs
@@ -50,7 +50,7 @@ namespace misas_thai_api
                     .BearerAuthCredentials(
                         new BearerAuthModel.Builder(accessToken).Build()
                     )
-                    .Environment(Square.Environment.Sandbox)
+                    .Environment(Square.Environment.Production)
                     .Build();
 
                 var money = new Money(
